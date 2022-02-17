ac01bac577c4fb0b2ca11b1e8a8328ead5641dee
Sean <seanthewonderful@gmail.com>
2022-02-17 10:20:51 -0700

    commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,8 +1,37 @@
 // Write your code below
+// function isPalindrome(str) {
+//     var re = /[\W_]/g
+//     var lowRegStr = str.toLowerCase().replace(re, '')
+//     var reverseStr = lowRegStr.split('').reverse().join('')
+//     console.log(lowRegStr)
+//     console.log(reverseStr)
+//     return reverseStr===lowRegStr
+// }
+// console.log(isPalindrome("A man, a plan, a canal. Panama"))
+
+// function isPalindrome(str) {
+//     var lowRegStr = str.toLowerCase().replace('')
+//     var reverseStr = lowRegStr.split('').reverse().join('')
+//     console.log(lowRegStr)
+//     console.log(reverseStr)
+//     return reverseStr===lowRegStr
+// }
+// console.log(isPalindrome("Racecar"))
+
 function isPalindrome(str) {
-    var re = /[\W_]/g
-    var lowRegStr = str.toLowerCase().replace(re, '')
-    var reverseStr = lowRegStr.split('').reverse().join('')
-    return reverseStr===lowRegStr
+    let splitStr = str.split('')
+    let reverseArr = splitStr.reverse()
+    let joinArr = reverseArr.join('')
+    //console.log(joinArr)
+    return str === joinArr
 }
-console.log(isPalindrome("A man, a plan, a canal. Panama"))
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('racecars'))
+
+// function isPalindrome(str){
+//     let newStr = ''
+//     for(i=str.length; i<=0; i--){
+//         newStr += newStr[i]
+//     }console.log(newStr)
+// }
+// console.log(isPalindrome('racecar'))
